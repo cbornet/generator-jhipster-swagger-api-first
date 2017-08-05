e60f031997ea6ec4e197a221ab4001b2280e80bf
cbornet <cbornet@hotmail.com>
2017-08-05 12:22:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# generator-jhipster-swagger-api-first
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> JHipster module, JHipster module to support API first development using swagger
+
+# Introduction
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://jhipster.github.io/installation.html)
+
+# Installation
+
+## With Yarn
+
+To install this module:
+
+```bash
+yarn global add generator-jhipster-swagger-api-first
+```
+
+To update this module:
+
+```bash
+yarn global upgrade generator-jhipster-swagger-api-first
+```
+
+## With NPM
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-swagger-api-first
+```
+
+To update this module:
+
+```bash
+npm update -g generator-jhipster-swagger-api-first
+```
+
+# Usage
+
+# License
+
+Apache-2.0 © [Christophe Bornet](https://github.com/cbornet)
+
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-swagger-api-first.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-swagger-api-first
+[travis-image]: https://travis-ci.org/cbornet/generator-jhipster-swagger-api-first.svg?branch=master
+[travis-url]: https://travis-ci.org/cbornet/generator-jhipster-swagger-api-first
+[daviddm-image]: https://david-dm.org/cbornet/generator-jhipster-swagger-api-first.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/cbornet/generator-jhipster-swagger-api-first
---
+++ generators/app/index.js
@@ -0,0 +1,151 @@
+const util = require('util');
+const chalk = require('chalk');
+const generator = require('yeoman-generator');
+const packagejs = require('../../package.json');
+const semver = require('semver');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
+
+const JhipsterGenerator = generator.extend({});
+util.inherits(JhipsterGenerator, BaseGenerator);
+
+module.exports = JhipsterGenerator.extend({
+    initializing: {
+        readConfig() {
+            this.jhipsterAppConfig = this.getJhipsterAppConfig();
+            if (!this.jhipsterAppConfig) {
+                this.error('Can\'t read .yo-rc.json');
+            }
+        },
+        displayLogo() {
+            // it's here to show that you can use functions from generator-jhipster
+            // this function is in: generator-jhipster/generators/generator-base.js
+            this.printJHipsterLogo();
+
+            // Have Yeoman greet the user.
+            this.log(`\nWelcome to the ${chalk.bold.yellow('JHipster swagger-api-first')} generator! ${chalk.yellow(`v${packagejs.version}\n`)}`);
+        },
+        checkJhipster() {
+            const jhipsterVersion = this.jhipsterAppConfig.jhipsterVersion;
+            const minimumJhipsterVersion = packagejs.dependencies['generator-jhipster'];
+            if (!semver.satisfies(jhipsterVersion, minimumJhipsterVersion)) {
+                this.warning(`\nYour generated project used an old JHipster version (${jhipsterVersion})... you need at least (${minimumJhipsterVersion})\n`);
+            }
+        }
+    },
+
+    /*prompting() {
+        const prompts = [
+            {
+                type: 'input',
+                name: 'message',
+                message: 'Please put something',
+                default: 'hello world!'
+            }
+        ];
+
+        const done = this.async();
+        this.prompt(prompts).then((props) => {
+            this.props = props;
+            // To access props later use this.props.someOption;
+
+            done();
+        });
+    },*/
+
+    writing() {
+        // function to use directly template
+        this.template = function (source, destination) {
+            this.fs.copyTpl(
+                this.templatePath(source),
+                this.destinationPath(destination),
+                this
+            );
+        };
+
+        // read config from .yo-rc.json
+        this.baseName = this.jhipsterAppConfig.baseName;
+        this.packageName = this.jhipsterAppConfig.packageName;
+        this.packageFolder = this.jhipsterAppConfig.packageFolder;
+        this.clientFramework = this.jhipsterAppConfig.clientFramework;
+        this.clientPackageManager = this.jhipsterAppConfig.clientPackageManager;
+        this.buildTool = this.jhipsterAppConfig.buildTool;
+
+        // use function in generator-base.js from generator-jhipster
+        this.angularAppName = this.getAngularAppName();
+
+        // use constants from generator-constants.js
+        const javaDir = `${jhipsterConstants.SERVER_MAIN_SRC_DIR + this.packageFolder}/`;
+        const resourceDir = jhipsterConstants.SERVER_MAIN_RES_DIR;
+        const webappDir = jhipsterConstants.CLIENT_MAIN_SRC_DIR;
+
+        // variable from questions
+        //this.message = this.props.message;
+
+        if (this.buildTool === 'maven') {
+            let executions = '                ' +
+                '<executions>' + '\n                ' +
+                '    <execution>' + '\n                ' +
+                '        <goals>' + '\n                ' +
+                '            <goal>generate</goal>' + '\n                ' +
+                '        </goals>' + '\n                ' +
+                '        <configuration>' + '\n                ' +
+                '            <inputSpec>src/main/resources/swagger/api.yml</inputSpec>' + '\n                ' +
+                '            <language>spring</language>' + '\n                ' +
+                `            <apiPackage>${this.packageName}.web.api.controller</apiPackage>` + '\n                ' +
+                `            <modelPackage>${this.packageName}.web.api.model</modelPackage>` + '\n                ' +
+                '            <generateSupportingFiles>false</generateSupportingFiles>' + '\n                ' +
+                '            <configOptions>' + '\n                ' +
+                '                <interfaceOnly>true</interfaceOnly>' + '\n                ' +
+                '                <java8>true</java8>' + '\n                ' +
+                '            </configOptions>' + '\n                ' +
+                '        </configuration>' + '\n                ' +
+                '    </execution>' + '\n                ' +
+                '</executions>'
+            
+
+            this.addMavenPlugin('io.swagger', 'swagger-codegen-maven-plugin', '2.2.3', executions)
+            this.template('_api.yml', resourceDir + 'swagger/api.yml');
+        }
+
+
+        /*try {
+            this.registerModule('generator-jhipster-swagger-api-first', 'entity', 'post', 'entity', 'JHipster module to support API first development using swagger');
+        } catch (err) {
+            this.log(`${chalk.red.bold('WARN!')} Could not register as a jhipster entity post creation hook...\n`);
+        }*/
+    },
+
+    /*install() {
+        let logMsg =
+            `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install`)}`;
+
+        if (this.clientFramework === 'angular1') {
+            logMsg =
+                `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install & bower install`)}`;
+        }
+        const injectDependenciesAndConstants = (err) => {
+            if (err) {
+                this.warning('Install of dependencies failed!');
+                this.log(logMsg);
+            } else if (this.clientFramework === 'angular1') {
+                this.spawnCommand('gulp', ['install']);
+            }
+        };
+        const installConfig = {
+            bower: this.clientFramework === 'angular1',
+            npm: this.clientPackageManager !== 'yarn',
+            yarn: this.clientPackageManager === 'yarn',
+            callback: injectDependenciesAndConstants
+        };
+        if (this.options['skip-install']) {
+            this.log(logMsg);
+        } else {
+            this.installDependencies(installConfig);
+        }
+    },*/
+
+    end() {
+        this.log('End of swagger-api-first generator');
+    }
+});
